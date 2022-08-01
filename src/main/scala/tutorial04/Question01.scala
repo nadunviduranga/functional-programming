6dea26affc9574ae6db890fc57552348de06c681
= <=>
2022-08-02 00:36:49 +0530

    initial commit 3

--- src/main/scala/tutorial04/Question01.scala
+++ src/main/scala/tutorial04/Question01.scala
@@ -14,6 +14,7 @@ object Question01 extends App {
     case x if x <= 200000 => x*0.04;
     case x if x <= 2000000 => x*0.035;
     case x if x > 2000000 => x* 0.065;
+    case x if x > 2000000 => x* 0.065;
   }
 
   // calculate total amount after year
